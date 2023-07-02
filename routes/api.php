309a25dd1111d0a6d7d9f2367ce3576da08cf7dc
Ahmed Maher Ali Al-Maqtari <88376540+Mr-NumberOne@users.noreply.github.com>
2023-07-03 02:46:46 +0300

    Lect8 - 1&2 -  APIs
    It does not seem to work, it keeps returning a 500 Error

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return auth()->user();
 });
+
+//Route::group()
+
+Route::post('/register', [AuthUserController::class, 'register'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/login', [AuthUserController::class, 'login'])
+    ->middleware('guest')
+    ->name('login');
+
+
+Route::post('/logout', [AuthUserController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+//
+Route::get('products',[ProductController::class,'index']);
+Route::middleware(['auth:sanctum'])
+    ->resource('products',
+        ProductController::class)
+    ->except(['index']);
+//Route::middleware(['auth:sanctum'])->resource('products',ProductController::class);
